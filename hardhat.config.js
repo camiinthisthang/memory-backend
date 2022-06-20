a0c1168182cb11ec98a846476ecbd30e72de3c8a
Camila Ramos <camiinthisthang@Camilas-MacBook-Pro-3.local>
2022-06-20 16:46:43 -0700

    deployed and verified contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-require("hardhat-tracer");
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html

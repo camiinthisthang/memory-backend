ac111c3312892b3dcfbb00170cfe5e910f26bf2c
Camila Ramos <camiinthisthang@Camilas-MacBook-Pro-3.local>
2022-06-23 10:34:58 -0700

    added logic to add friends

--- scripts/run.js
+++ scripts/run.js
@@ -3,17 +3,17 @@
 const hre = require("hardhat");
 
 const main = async () => {
-    const memoryContractFactory = await hre.ethers.getContractFactory("Scrapbook");
+    const memoryContractFactory = await hre.ethers.getContractFactory("Memory");
     const memoryContract = await memoryContractFactory.deploy();
     await memoryContract.deployed();
     console.log("contract deployed to:", memoryContract.address);
 
     let memCID = "bafybeihe2gh5zypdiacmz5zl7z3wuhohlepjwysjkbzar5wgaopr4nwqyi"
     let eventTimeStamp = 1952517724
-    let memoryId = 1;
     let visible = false;
+    let friends = ["0x8A9cF9dABAC03273Ae81c7BB1029edabfB97550a"];
 
-    let txn = await memoryContract.createNewMemory(memCID, eventTimeStamp, memoryId, visible);
+    let txn = await memoryContract.createNewMemory(memCID, eventTimeStamp, visible, friends);
     let wait = await txn.wait();
     console.log("NEW MEMORY CREATED, console log from the run script", wait.events[0].event, wait.events[0].args)
 
